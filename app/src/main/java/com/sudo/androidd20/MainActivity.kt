8a499954c9b4bf06ea71cd0cfe0539989432dcc6
S4ltF1sh <84552830+S4ltF1sh@users.noreply.github.com>
2022-07-27 19:00:54 +0700

    Revert "Lq dung"

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -2,28 +2,10 @@ package com.sudo.androidd20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
-import com.sudo.androidd20.data.User
-import com.sudo.androidd20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-        val userReceived = intent?.extras?.get("userData") as User
-
-        binding.tvUserName.text = userReceived.userName
-        binding.tvName.text = userReceived.name
-        binding.tvAge.text = userReceived.age.toString()
-
-        binding.tvBtnLogOut.setOnClickListener {
-            finish()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
