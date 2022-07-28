fafa385d8466b3eb52382d8733c28e53e99eeaa0
Tuan Vo <dungtuan095@gmail.com>
2022-07-28 21:13:15 +0700

    Commit UI

--- app/src/main/java/com/sudo/androidd20/LoginFragment.kt
+++ app/src/main/java/com/sudo/androidd20/LoginFragment.kt
@@ -35,7 +35,7 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater,container,false)
         return binding.root
     }
