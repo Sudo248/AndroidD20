d0f87711d0d105388a8480e71f118ea538772446
thuanpham582002 <tienthuan05082002@gmail.com>
2022-07-28 18:03:57 +0700

    first cm

--- app/src/main/java/com/sudo/androidd20/FragmentLogin.kt
+++ app/src/main/java/com/sudo/androidd20/FragmentLogin.kt
@@ -1,19 +1,12 @@
 package com.sudo.androidd20
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.sudo.androidd20.databinding.LoginFragmentBinding
-import org.w3c.dom.Text
 
 class FragmentLogin : Fragment() {
     private var callBackFragment: CallBackFragment? = null
