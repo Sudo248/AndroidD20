ef3f11f029ffcec0b3fb51e76745afd9354a39f9
Sudo248 <hongduong080501@gmail.com>
2022-07-30 00:43:06 +0700

    [UPDATE] fragment login and sign up

--- app/src/main/java/com/sudo/androidd20/activities/AuthActivity.kt
+++ app/src/main/java/com/sudo/androidd20/activities/AuthActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.sudo.androidd20.Navigator
 import com.sudo.androidd20.R
+import com.sudo.androidd20.User
 import com.sudo.androidd20.fragments.FragmentLogin
 
 class AuthActivity : AppCompatActivity() {
+
+    val listUser: MutableList<User> = mutableListOf(
+        User("Duong", "123"),
+        User("Minh", "456")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -15,15 +22,5 @@ class AuthActivity : AppCompatActivity() {
             .of(supportFragmentManager)
             .add(FragmentLogin::class.java)
 
-    }
-
-    fun navigate(fClass: Class<out Fragment>) {
-        Navigator
-            .of(supportFragmentManager)
-            .navigate(fClass)
-    }
-
-    fun pop() {
-        Navigator.of(supportFragmentManager).back()
     }
 }
--- app/src/main/java/com/sudo/androidd20/fragments/FragmentSignUp.kt
+++ app/src/main/java/com/sudo/androidd20/fragments/FragmentSignUp.kt
@@ -5,20 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import com.sudo.androidd20.Constants
 import com.sudo.androidd20.Navigator
 import com.sudo.androidd20.R
+import com.sudo.androidd20.User
 import com.sudo.androidd20.activities.AuthActivity
 import com.sudo.androidd20.databinding.FragmentSignUpBinding
 
 
 class FragmentSignUp : Fragment() {
 
-    private lateinit var binding: FragmentSignUpBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    companion object {
+        const val FRAGMENT_SIGNUP_LISTENER = "FRAGMENT_SIGNUP_LISTENER"
     }
+
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,15 +30,57 @@ class FragmentSignUp : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
-        binding.txtToLogin.setOnClickListener {
-//            (requireActivity() as AuthActivity).pop()
-            Navigator.of(parentFragmentManager).back()
-        }
+        setOnClickListener()
         return binding.root
     }
 
-    private fun setOnClick() {
-
+    private fun setOnClickListener() {
+        with(binding) {
+            txtToLogin.setOnClickListener {
+                Navigator.of(parentFragmentManager).navigate(FragmentLogin::class.java)
+            }
+            btnSignup.setOnClickListener {
+                onSignUpClickListener()
+            }
+        }
     }
 
+    private fun onSignUpClickListener() {
+        if (isSamePassWord()) {
+            val user = getInputUser()
+            if (isInvalidUser(user)) {
+                addUserAndBackLogin(user)
+            } else {
+                showToast("Invalid User")
+            }
+        } else {
+            showToast("Wrong password")
+        }
+    }
+
+    private fun getInputUser(): User {
+        val email = binding.tilEmail.editText?.text.toString()
+        val password = binding.tilPassword.editText?.text.toString()
+        return User(email, password)
+    }
+
+    private fun isSamePassWord(): Boolean {
+        val password = binding.tilPassword.editText?.text.toString()
+        val confirmPassword = binding.tilConfirmPassword.editText?.text.toString()
+        return password == confirmPassword
+    }
+
+    private fun isInvalidUser(user: User): Boolean {
+        return !(activity as AuthActivity).listUser.contains(user)
+    }
+
+    private fun addUserAndBackLogin(user: User) {
+        (activity as AuthActivity).listUser.add(user)
+        setFragmentResult(FragmentLogin.FRAGMENT_LOGIN_LISTENER, bundleOf(Constants.USER to user))
+        Navigator.of(parentFragmentManager).back()
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
 }
