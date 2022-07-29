deef70d482f2c9fd68c8345ea815917941c6dbbe
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-07-29 22:29:54 +0700

    test 3

---
+++ app/src/main/java/com/sudo/androidd20/FirstFragment.kt
@@ -0,0 +1,11 @@
+package com.sudo.androidd20
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.sudo.androidd20
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        val sign_up_or_in = findViewById<Button>(R.id.sign_up_or_in)
+        val have_account_or_not = findViewById<TextView>(R.id.have_account_or_not)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flfragment, firstFragment)
+            commit()
+        }
+        sign_up_or_in.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                if(sign_up_or_in.text=="Sign up"){
+                    sign_up_or_in.text="Log in"
+                    have_account_or_not.text="Already, have an account?"
+                    replace(R.id.flfragment, secondFragment)
+                    addToBackStack(null)
+                    commit()
+                }
+                else{
+                    sign_up_or_in.text="Sign up"
+                    have_account_or_not.text="Don't have account?"
+                    replace(R.id.flfragment, firstFragment)
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+        }
     }
 }
