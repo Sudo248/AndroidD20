5e6e80aa376fa2d2852f1a547c2c3d4d49ca6018
Tuan Vo <dungtuan095@gmail.com>
2022-07-29 10:30:51 +0700

    Chưa xử lý Data

--- app/src/main/java/com/sudo/androidd20/SecondActivity.kt
+++ app/src/main/java/com/sudo/androidd20/SecondActivity.kt
@@ -6,12 +6,12 @@ import com.sudo.androidd20.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
-    private val user = intent?.extras?.get("userData") as User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.username.text = user.email
+        val user = intent?.extras?.get("userData") as User
+        binding.username.setText(user.email)
     }
 }
