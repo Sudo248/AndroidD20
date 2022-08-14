c27567ce1d8fc1b9916d16c4a37ff5d17b787992
Tuan Vo <dungtuan095@gmail.com>
2022-08-14 22:18:50 +0700

    First commit

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.sudo.androidd20
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sudo.androidd20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: MyBroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        receiver = MyBroadcastReceiver()
+        /*
+            IntentFilter rất hữu ích để xác định ứng dụng nào muốn nhận ý định nào,
+            vì ở đây chúng tôi muốn phản hồi việc thay đổi chế độ trên máy bay
+             */
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            /*
+            đăng ký tham số receiver it được truyền vào hàm registerReceiver () là IntentFilter mà chúng ta vừa tạo
+             */
+            registerReceiver(receiver,it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
--- app/src/main/java/com/sudo/androidd20/MyBroadcastReceiver.kt
+++ app/src/main/java/com/sudo/androidd20/MyBroadcastReceiver.kt
@@ -3,12 +3,17 @@ package com.sudo.androidd20
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.widget.Toast
 
 
 class MyBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
-        Log.d("TAG", "Air Plane mode")
+        val isAirplaneModeEnabled = p1?.getBooleanExtra("state", false) ?: return
+        if (isAirplaneModeEnabled) {
+            Toast.makeText(p0, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(p0, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+        }
     }
 }
