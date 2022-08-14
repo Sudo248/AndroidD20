148e1e71fcb9f44694ae41a5d4420283cfb48d44
Tuan Vo <dungtuan095@gmail.com>
2022-08-14 11:55:56 +0700

    First commit

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sudo.androidd20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sudo.androidd20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
