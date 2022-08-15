c8ab88490a1810df775c292c488b40389271cc88
VuNAP <vuvovan2k2>
2022-08-15 22:44:01 +0700

    [ADD] add location

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.sudo.androidd20
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.Location
 import android.net.ConnectivityManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.PermissionRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.sudo.androidd20.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var broadcastReceiver: BroadcastReceiver
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        registerReceiver()
+
+        requestPermission()
+        getLocation()
+    }
+
+    private fun requestPermission() {
+        val locationPermissionRequest =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    when {
+                        permissions.getOrDefault(
+                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            false
+                        ) -> {
+
+                        }
+                        permissions.getOrDefault(
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            false
+                        ) -> {
+                            // Only approximate location access granted.
+                        }
+                        else -> {
+                            // No location access granted.
+                        }
+                    }
+                }
+            }
+        locationPermissionRequest.launch(
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            binding.tvCurrentLocation.text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
+        }
+    }
+
+    private fun registerReceiver() {
         broadcastReceiver = MyBroadcastReceiver()
         val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         registerReceiver(broadcastReceiver, filter)
@@ -22,6 +90,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        binding.tvPer.text = (broadcastReceiver as MyBroadcastReceiver).per.toString()
+
     }
 }
