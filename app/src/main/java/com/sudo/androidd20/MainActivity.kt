8065bf86ccf53c1dc5550aeb44dd8ee620296b5b
Tuan Vo <dungtuan095@gmail.com>
2022-08-16 02:10:37 +0700

    First commit

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sudo.androidd20
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -10,14 +13,12 @@ import com.sudo.androidd20.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var receiver: MyBroadcastReceiver
-    private lateinit var mBatteryReceiver: BatteryReceiver
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         receiver = MyBroadcastReceiver()
-        mBatteryReceiver = BatteryReceiver()
         /*
             IntentFilter rất hữu ích để xác định ứng dụng nào muốn nhận ý định nào,
             vì ở đây chúng tôi muốn phản hồi việc thay đổi chế độ trên máy bay
@@ -32,9 +33,19 @@ class MainActivity : AppCompatActivity() {
             registerReceiver(mBatteryReceiver,it)
         }
     }
+    private val mBatteryReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            val level = p1?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val batteryShow = "Phần trăm pin là $level %"
+            binding.tvBatteryLevel.text = batteryShow
+        }
+
+    }
 
     override fun onStop() {
         super.onStop()
         unregisterReceiver(receiver)
+        unregisterReceiver(mBatteryReceiver)
     }
 }
+
