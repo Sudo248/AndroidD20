0e00942922ac6548ccb5fed3b43ab11664de1945
Tuan Vo <dungtuan095@gmail.com>
2022-08-16 02:15:45 +0700

    First commit

--- app/src/main/java/com/sudo/androidd20/MainActivity.kt
+++ app/src/main/java/com/sudo/androidd20/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.BatteryManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.sudo.androidd20.databinding.ActivityMainBinding
 
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
         receiver = MyBroadcastReceiver()
         /*
             IntentFilter rất hữu ích để xác định ứng dụng nào muốn nhận ý định nào,
@@ -30,9 +32,41 @@ class MainActivity : AppCompatActivity() {
             registerReceiver(receiver,it)
         }
         IntentFilter(Intent.ACTION_BATTERY_CHANGED).also {
-            registerReceiver(mBatteryReceiver,it)
+            registerReceiver(this.mBatteryReceiver,it)
+        }
+        binding.btRequestLocation.setOnClickListener {
+            getLocation()
+        }
+        setContentView(binding.root)
+    }
+
+    private fun getLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+            return
+        }
+        task.addOnSuccessListener {
+            val locationStr = "Latitude: ${it.latitude} Longitude:${it.longitude}"
+            if (it != null)
+                binding.tvLocationGranted.text = locationStr
         }
     }
+
     private val mBatteryReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(p0: Context?, p1: Intent?) {
             val level = p1?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
