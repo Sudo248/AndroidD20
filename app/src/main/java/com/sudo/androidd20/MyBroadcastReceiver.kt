4ab0a6c3850ead5466e513c19b29cef6ae46de64
VuNAP <vuvovan2k2>
2022-08-15 22:43:33 +0700

    [ADD] add broadcast receiver

--- app/src/main/java/com/sudo/androidd20/MyBroadcastReceiver.kt
+++ app/src/main/java/com/sudo/androidd20/MyBroadcastReceiver.kt
@@ -4,10 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.widget.Toast
 
 class MyBroadcastReceiver : BroadcastReceiver() {
     var per: Int = 0
     override fun onReceive(p0: Context?, p1: Intent?) {
         per = p1?.getIntExtra(BatteryManager.EXTRA_LEVEL, 100)!!
+        Toast.makeText(p0!!, "Battery $per", Toast.LENGTH_LONG).show()
     }
 }
